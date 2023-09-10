e6f9ef2528c71b53533b852a0954a73cbdc05e06
ZzzHe <110341553+ZzzHe2333@users.noreply.github.com>
2023-09-10 08:38:28 +0800

    Update Constant.java

--- app/src/main/java/com/giftedcat/adskiphelper/config/Constant.java
+++ app/src/main/java/com/giftedcat/adskiphelper/config/Constant.java
@@ -11,8 +11,28 @@ public class Constant {
     public static final String SCAN_KEYWORD = "跳过";
 
     /**
+    * 界面上查找的文字
+     * */
+    public static final String SCAN_KEYWORD_sign = "签到";
+
+    /**
+    * 界面上查找的文字
+     * */
+    public static final String SCAN_KEYWORD_sign_nei = "内勤签到";
+
+    /**
+    * 界面上查找的文字
+     * */
+    public static final String SCAN_KEYWORD_sign_wai = "外勤签到";
+    
+    /**
      * 默认扫描时间4秒
      * */
     public static final long SCAN_TIME = 4 * 1000L;
+
+     /**
+     * 默认上班时间为8时
+     * */
+    public static final long SCAN_TIME = 8;   
 
 }
